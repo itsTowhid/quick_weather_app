a1665589aed02d4771e9b771eb893fa8e941d038
Towhid <95753091+itsTowhid@users.noreply.github.com>
2024-07-14 01:21:26 +0600

    api call and view data implemented in home_view.dart and home_controller.dart

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,52 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quick_weather_app/app/modules/home/service/weather_exception.dart';
+import 'package:quick_weather_app/app/modules/home/service/weather_service.dart';
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
+class HomeController extends GetxController
+    with StateMixin<Map<String, dynamic>> {
+  HomeController(this.repo);
 
-  final count = 0.obs;
+  final cityController = TextEditingController(text: 'dhaka');
+  final history = <String, dynamic>{};
+  final isLoading = false.obs;
+  final WeatherService repo;
+
   @override
   void onInit() {
     super.onInit();
+    change(state, status: RxStatus.empty());
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  void searchWeather() async {
+    final city = cityController.text;
+    if (history.containsKey(city)) {
+      return change(history[city], status: RxStatus.success());
+    }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+    change(null, status: RxStatus.loading());
+    Map<String, dynamic>? responseData;
+    isLoading.value = true;
+    String? errMsg;
 
-  void increment() => count.value++;
+    if (cityController.text.isEmpty) {
+      errMsg = 'City name cant be empty';
+    } else {
+      try {
+        final responseData = await repo.fetchWeather(city);
+        change(responseData, status: RxStatus.success());
+        history[city] = responseData;
+        isLoading.value = false;
+        return;
+      } on CityNotFoundException {
+        errMsg = 'City not found. Please check the city name.';
+      } on WeatherServiceException catch (e) {
+        errMsg = e.message;
+      } catch (e) {
+        errMsg = '$e\nPlease try again';
+      }
+    }
+    isLoading.value = false;
+    change(responseData, status: RxStatus.error(errMsg));
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,24 +1,83 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      appBar: AppBar(title: const Text('Quick Weather App'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: controller.obx(
+                  (state) => buildWeatherDataView(state),
+                  onEmpty: const Text('Enter your city name'),
+                  onError: (error) => Text(
+                    error ?? 'Something went wrong',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    onSubmitted: (_) => controller.searchWeather(),
+                    controller: controller.cityController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'City Name',
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Obx(
+                    () => controller.isLoading.value
+                        ? const CircularProgressIndicator()
+                        : ElevatedButton(
+                            onPressed: controller.searchWeather,
+                            child: const Text('Search'),
+                          ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Column buildWeatherDataView(Map<String, dynamic>? state) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          state?['name'],
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '${(state?['main']['temp'] - 273.15).toStringAsFixed(2)} °C',
+          style: const TextStyle(fontSize: 20),
+        ),
+        Text(
+          state?['weather'][0]['description'],
+          style: const TextStyle(fontSize: 20),
+        ),
+        Image.network(
+          'http://openweathermap.org/img/w/${state?['weather'][0]['icon']}.png',
+        ),
+      ],
     );
   }
 }
