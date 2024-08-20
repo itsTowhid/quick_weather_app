2d220b3584ebea6fd5cad8950c2a62ea950c804e
Towhid <95753091+itsTowhid@users.noreply.github.com>
2024-08-20 15:46:24 +0600

    apk release workflow changes

--- lib/app/core/network/connectivity_service.dart
+++ lib/app/core/network/connectivity_service.dart
@@ -1,13 +1,14 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityService {
-  final Connectivity _connectivity = Connectivity();
+  // final Connectivity _connectivity = Connectivity();
 
   Future<bool> isConnected() async {
-    var connectivityResult = await _connectivity.checkConnectivity();
-    return !connectivityResult.contains(ConnectivityResult.none);
+    // var connectivityResult = await _connectivity.checkConnectivity();
+    // return !connectivityResult.contains(ConnectivityResult.none);
+    return true;
   }
 
-  Stream<List<ConnectivityResult>> get onConnectivityChanged =>
-      _connectivity.onConnectivityChanged;
+  // Stream<List<ConnectivityResult>> get onConnectivityChanged =>
+  //     _connectivity.onConnectivityChanged;
 }
